e694dc32f65c85230899fd7e02a221abbc7b1b6c
角居ひなの <nana121020@kadoihinanonoMacBook-Air.local>
2023-06-20 19:04:21 +0900

    画像も必須に変更

--- app/models/prototype.rb
+++ app/models/prototype.rb
@@ -2,6 +2,7 @@ class Prototype < ApplicationRecord
   validates :title, presence: true
   validates :catch_copy, presence: true
   validates :concept, presence: true
+  validates :image, presence: true
   #devise :database_authenticatable, :registerable,
          #:recoverable, :rememberable, :validatable
 
